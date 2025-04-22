979368b3ca82749b5eb5baf791ca46e57d61f655
Ganghui <leeganghee4954@naver.com>
2025-04-22 17:18:29 +0900

    폴더 합치기

--- lib/widgets/bottom_navigation.dart
+++ lib/widgets/bottom_navigation.dart
@@ -1,130 +1,144 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:meongjup/pages/adoption_detail%20copy.dart';
-import 'package:meongjup/pages/adoption_detail.dart';
-import 'package:meongjup/pages/adoption_list%20copy.dart';
 import 'package:meongjup/pages/adoption_list.dart';
+
 class BottomNavigation extends StatelessWidget {
   final int selectedIndex;
 
-  const BottomNavigation({
-    super.key, 
-    required this.selectedIndex,
-  });
+  const BottomNavigation({super.key, required this.selectedIndex});
 
   void navigateToPage(BuildContext context, int index) {
     switch (index) {
       case 0:
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MainPage()));
+        Navigator.of(
+          context,
+        ).pushReplacement(MaterialPageRoute(builder: (context) => MainPage()));
         break;
-      case 1:  
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MainPageCopy()));
+      case 1:
+        // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MainPageCopy()));
         break;
       case 2:
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => detail()));
+        // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Null()));
         break;
       case 3:
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => detailcopy()));
+        // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => detailcopy()));
         break;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-          SizedBox(
-            width: 60,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  constraints: BoxConstraints(),
-                  icon: selectedIndex == 0 ? 
-                    Image.asset('assets/images/home.png',
-                      width: 24,
-                      height: 24,) : 
-                    Image.asset('assets/images/home_1.png',
-                      width: 24,
-                      height: 24,
-                    ),
-                  onPressed: () => navigateToPage(context, 0),
-                ),
-                Text('입양', style: TextStyle(fontSize: 12)),
-              ],
+            SizedBox(
+              width: 60,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    constraints: BoxConstraints(),
+                    icon:
+                        selectedIndex == 0
+                            ? Image.asset(
+                              'assets/images/home.png',
+                              width: 24,
+                              height: 24,
+                            )
+                            : Image.asset(
+                              'assets/images/home_1.png',
+                              width: 24,
+                              height: 24,
+                            ),
+                    onPressed: () => navigateToPage(context, 0),
+                  ),
+                  Text('입양', style: TextStyle(fontSize: 12)),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            width: 60,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  constraints: BoxConstraints(),
-                  icon: 
-                    selectedIndex == 1 ? 
-                    Image.asset('assets/images/icon2.png',
-                      width: 24,
-                      height: 24) : 
-                    Image.asset('assets/images/icon2_1.png',
-                      width: 24,
-                      height: 24
-                    ),
-                  onPressed: () => navigateToPage(context, 1),
-                ),
-                Text('실종', style: TextStyle(fontSize: 12)),
-              ],
+            SizedBox(
+              width: 60,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    constraints: BoxConstraints(),
+                    icon:
+                        selectedIndex == 1
+                            ? Image.asset(
+                              'assets/images/icon2.png',
+                              width: 24,
+                              height: 24,
+                            )
+                            : Image.asset(
+                              'assets/images/icon2_1.png',
+                              width: 24,
+                              height: 24,
+                            ),
+                    onPressed: () => navigateToPage(context, 1),
+                  ),
+                  Text('실종', style: TextStyle(fontSize: 12)),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            width: 60,
-            child: Column(
-              mainAxisSize: MainAxisSize.min, 
-              children: [
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  constraints: BoxConstraints(),
-                  icon: 
-                    selectedIndex == 2 ? 
-                    Image.asset('assets/images/icon3.png',
-                      width: 24,
-                      height: 24) : 
-                    Image.asset('assets/images/icon3_1.png',
-                      width: 24,
-                      height: 24),
-                  onPressed: () => navigateToPage(context, 2),
-                ),
-                Text('자원봉사', style: TextStyle(fontSize: 12)),
-              ],
+            SizedBox(
+              width: 60,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    constraints: BoxConstraints(),
+                    icon:
+                        selectedIndex == 2
+                            ? Image.asset(
+                              'assets/images/icon3.png',
+                              width: 24,
+                              height: 24,
+                            )
+                            : Image.asset(
+                              'assets/images/icon3_1.png',
+                              width: 24,
+                              height: 24,
+                            ),
+                    onPressed: () => navigateToPage(context, 2),
+                  ),
+                  Text('자원봉사', style: TextStyle(fontSize: 12)),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            width: 60,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  constraints: BoxConstraints(),
-                  icon: 
-                    selectedIndex == 3 ? 
-                    Image.asset('assets/images/icon4.png',
-                      width: 24,
-                      height: 24) : 
-                    Image.asset('assets/images/icon4_1.png',
-                      width: 24,
-                      height: 24),
-                  onPressed: () => navigateToPage(context, 3),
-                ),
-                Text('멍피드', style: TextStyle(fontSize: 12)),
-              ],
+            SizedBox(
+              width: 60,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    constraints: BoxConstraints(),
+                    icon:
+                        selectedIndex == 3
+                            ? Image.asset(
+                              'assets/images/icon4.png',
+                              width: 24,
+                              height: 24,
+                            )
+                            : Image.asset(
+                              'assets/images/icon4_1.png',
+                              width: 24,
+                              height: 24,
+                            ),
+                    onPressed: () => navigateToPage(context, 3),
+                  ),
+                  Text('멍피드', style: TextStyle(fontSize: 12)),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
