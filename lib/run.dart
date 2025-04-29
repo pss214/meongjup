51609634fc2bce12ef539afe79c907e3933866d7
Ganghui <leeganghee4954@naver.com>
2025-04-29 16:52:53 +0900

    fix: runApp 홈화면 코드 수정

--- lib/run.dart
+++ lib/run.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meongjup/pages/adoption_list.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:meongjup/pages/volunteer_detail.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp(home: VolunteerDetail()));
+  runApp(MaterialApp(home: MainPage()));
 }
