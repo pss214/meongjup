3796fe87b1cdd74883ca396f7af676730d744494
bonasuuu <sunny010950@gmail.com>
2025-04-30 17:23:06 +0900

    feat:앱로고 및 스플래쉬

--- lib/run.dart
+++ lib/run.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:meongjup/pages/adoption_list.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(home: MainPage()));
