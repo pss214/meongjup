e3f2df4cc52dd02c46f338e8d9cbdcd076ac2a99
KDW0061 <rlaeodnjs0610@gmail.com>
2025-05-02 15:17:47 +0900

    fix: url 이동오류 수정

--- lib/pages/volunteer_detail.dart
+++ lib/pages/volunteer_detail.dart
@@ -108,11 +108,73 @@ class VolunteerDetail extends StatelessWidget {
                                 padding: EdgeInsets.fromLTRB(36, 12, 36, 12),
                                 child: GestureDetector(
                                   onTap: () async {
-                                    await launchUrl(
-                                      Uri.parse(
-                                        "https://news.seoul.go.kr/env/archives/560028",
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) => AlertDialog(
+                                        title: const Text(
+                                          '외부 페이지 URL로 이동합니다.',
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                        actionsPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                                        actions: [
+                                          Row(
+                                            children: [
+                                              Expanded(
+                                                child: TextButton(
+                                                  onPressed: () {
+                                                    Navigator.of(context).pop();
+                                                  },
+                                                  style: TextButton.styleFrom(
+                                                    backgroundColor: const Color(0xFFE0E0E0),
+                                                    shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(8),
+                                                    ),
+                                                  ),
+                                                  child: const Text(
+                                                    '취소',
+                                                    style: TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 15,
+                                                      fontWeight: FontWeight.w600,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                              const SizedBox(width: 10),
+                                              Expanded(
+                                                child: TextButton(
+                                                  onPressed: () async {
+                                                    await launchUrl(
+                                                      Uri.parse(
+                                                        "https://news.seoul.go.kr/env/archives/560028",
+                                                      ),
+                                                      mode: LaunchMode.externalApplication,
+                                                    );
+                                                  },
+                                                  style: TextButton.styleFrom(
+                                                    backgroundColor: const Color(0xFF75B1FF),
+                                                    shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(8),
+                                                    ),
+                                                  ),
+                                                  child: const Text(
+                                                    '확인',
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 15,
+                                                      fontWeight: FontWeight.w600,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          )
+                                        ],
                                       ),
-                                      mode: LaunchMode.externalApplication,
                                     );
                                   },
                                   child: Text(
